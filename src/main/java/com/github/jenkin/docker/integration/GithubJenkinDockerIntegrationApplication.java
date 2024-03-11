cca48f3fd2d366cf4cd232b6ff782a1934db040c
imarungupta <imarungupta@gmail.com>
2024-03-11 20:02:05 +0530

    First Commit

---
+++ src/main/java/com/github/jenkin/docker/integration/GithubJenkinDockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.jenkin.docker.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubJenkinDockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubJenkinDockerIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jenkin/docker/integration/GithubJenkinDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jenkin.docker.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubJenkinDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
