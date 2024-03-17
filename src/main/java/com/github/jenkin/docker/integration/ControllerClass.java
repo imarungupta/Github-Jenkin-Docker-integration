9625a4fe32b5b0e19d8053ffbf699643416820a5
imarungupta <imarungupta@gmail.com>
2024-03-17 22:53:56 +0530

    added K8s deplyed message method

--- src/main/java/com/github/jenkin/docker/integration/ControllerClass.java
+++ src/main/java/com/github/jenkin/docker/integration/ControllerClass.java
@@ -1,4 +1,13 @@
 package com.github.jenkin.docker.integration;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerClass {
+
+    @GetMapping("/message")
+    public String display(){
+        return "You have successfully deployed your docker image into K8S !!!";
+    }
 }
